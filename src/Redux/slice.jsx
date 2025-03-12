bcd4d2a62233ad7c278694e7f0e1e7cceea8d458
Amresh.Kumar <Amresh.Kumar@mantratec.com>
2025-03-12 15:58:00 +0530

    New Changes the redux

--- src/Redux/slice.jsx
+++ src/Redux/slice.jsx
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import deviceReducer from './DeviceSlice';
 import rightReducer from './RightsDistribution';
 
-
 export const store = configureStore({
   reducer: {
     device: deviceReducer,
